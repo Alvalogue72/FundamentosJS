65ca572f73f54174542cc1d1cb715e3412795649
Álvaro López <alvalogue72@gmail.com>
2024-10-10 23:40:15 +0200

    Prueba

--- js/logout.js
+++ js/logout.js
@@ -8,25 +8,9 @@ logout.addEventListener("click", function(event) {
     
 }); 
 
-/* const logout = document.getElementById("logout");
-
-logout.addEventListener("click", function(event) {
-  event.preventDefault();
-
-  deleteCookie("loggedIn");
-
-  const urlParams = new URLSearchParams(window.location.search);
-  const baseUrl = window.location.origin;
-  const queryParams = urlParams.toString();
-
-  // Redirigir al index.html con los mismos parámetros
-  window.location.href = `${baseUrl}?${queryParams}`;
-});
- */
-
-function redirigirAHome() {
+/* function redirigirAHome() {
   // Obtener la ruta actual del archivo HTML
-  const rutaActual = window.location.pathname;
+  const rutaActual = window.location.origin + window.location.pathname;
 
   // Dividir la ruta por '/' para obtener un array de directorios
   const partesRuta = rutaActual.split('/');
@@ -37,31 +21,23 @@ function redirigirAHome() {
   // Unir las partes restantes para obtener el directorio padre
   const directorioPadre = partesRuta.join('/');
 
+
+
   // Construir la ruta completa hacia el archivo index.html en el directorio raíz
-  const rutaHome = directorioPadre.split('/').slice(0, -1).join('/') + '/index.html';
+  const rutaHome = directorioPadre.split('/');
+
+const enga = rutaHome.indexOf("");
+
+const vamo = rutaHome.slice(0, enga + 2);
+
+const ultimo = vamo.join('/') + '/index.html';
 
   // Redirigir a la página de inicio
-  window.location.href = rutaHome;
-}
-/* function checkPath() {
-  
-  const rutaActual = window.location.pathname;
-
-  console.log(rutaActual)
-   
-  const partesRuta = rutaActual.split('/');
-  
-    // Eliminar el último elemento del array (nombre del archivo)
-    partesRuta.pop();
-  const levelsup = partesRuta.length;
-  
-    // Unir las partes restantes para obtener el directorio pad
-  
-   // Construir la ruta completa hacia el archivo index.html en el directorio raíz
-    const rutaHome = '../'.repeat(levelsup - 1) + 'index.html';
-
-    console.log(rutaHome);
-    
-    window.location.href = rutaHome;
-
+  window.location.href = ultimo;
 } */
+
+function redirigirAHome() {
+  // Redirigir directamente a la página de inicio en el directorio raíz
+  window.location.href = window.location.origin + '/index.html';
+}
+
